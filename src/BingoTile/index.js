62fe84e466248165710c3f7843553fd80bc3df66
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-10-06 17:08:11 -0700

    Get items in 5x5 grid

--- src/BingoTile/index.js
+++ src/BingoTile/index.js
@@ -3,7 +3,7 @@ import {Card} from 'react-bootstrap'
 
 function BingoTile ({challenge}) {
   return (
-    <Card>
+    <Card className="col-md-2 col-lg-2 col-xl-2 py-2">
       <Card.Title>{challenge.title}</Card.Title>
     </Card>
   )
--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import BingoTile from '../BingoTile'
 
-function Grid ({card, update}) {
+import { Container, Row } from 'react-bootstrap'
+
+function Grid({ card, update }) {
   return (
-    <div className="bingoGrid">
-      {Object.keys(card).map(id => <BingoTile key={id} challenge={card[id]}/>)}
-    </div>
+    <Container className="bingoGrid">
+      <Row>
+        {Object.keys(card).filter(id => id <= 5).map(id => <BingoTile key={id} challenge={card[id]} />)}
+      </Row>
+      <Row>
+        {Object.keys(card).filter(id => id > 5 && id <= 10).map(id => <BingoTile key={id} challenge={card[id]} />)}
+      </Row>
+      <Row>
+        {Object.keys(card).filter(id => id > 10 && id <= 15).map(id => <BingoTile key={id} challenge={card[id]} />)}
+      </Row>
+      <Row>
+        {Object.keys(card).filter(id => id > 15 && id <= 20).map(id => <BingoTile key={id} challenge={card[id]} />)}
+      </Row>
+      <Row>
+        {Object.keys(card).filter(id => id > 20 && id <= 25).map(id => <BingoTile key={id} challenge={card[id]} />)}
+      </Row>
+    </Container>
   )
 }
 
